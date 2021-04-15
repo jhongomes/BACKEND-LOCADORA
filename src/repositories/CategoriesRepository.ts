f0a4a5551c851f60f8b75b7985e8b5755dbb93ef
jhongomes <jhonatangomes859@gmail.com>
2021-04-15 15:37:23 -0400

    Adicionando metodo list categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -31,6 +31,10 @@ class CategoriesRepository {
         
          this.categories.push(category);  
     }
+
+    list(): Category[]{
+        return this.categories;
+    }
 }
 
 export { CategoriesRepository }
