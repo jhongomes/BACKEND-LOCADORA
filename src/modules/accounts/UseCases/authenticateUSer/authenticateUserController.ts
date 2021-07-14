284307410f810e03ac791fa0690dce714301e2b3
jhongomes <jhonatangomes859@gmail.com>
2021-07-14 17:28:45 -0400

    Criando autenticação de usuário

---
+++ src/modules/accounts/UseCases/authenticateUSer/authenticateUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { AuthenticateUserUseCase } from "./authenticateUserUseCase";
+
+
+class AuthenticateUserController {
+
+    async handle(request:Request, response:Response): Promise<Response>{
+        const { email, password } = request.body;
+
+        const authenticateUserUserCase = container.resolve(AuthenticateUserUseCase);
+
+        const token = await authenticateUserUserCase.execute({
+            email,
+            password
+        })
+
+
+        return response.json(token)
+    }
+    
+}
+
+export { AuthenticateUserController}
---
+++ src/modules/accounts/UseCases/authenticateUSer/authenticateUserUseCase.ts
@@ -0,0 +1,64 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+
+import { compare} from "bcryptjs"
+
+import { sign } from "jsonwebtoken"
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string,
+        email: string
+    },
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository : IUsersRepository
+    ){}
+
+    async execute({ email, password}: IRequest): Promise<IResponse>{
+
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user){
+            throw new Error("Email or password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch){
+            throw new Error("Email or password incorrect!");
+        }
+
+        const token = sign({}, "bdf6660e03046c8aaf84ba38cc72f1a3", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        return tokenReturn;
+
+        
+
+    }
+
+}
+
+export { AuthenticateUserUseCase}
