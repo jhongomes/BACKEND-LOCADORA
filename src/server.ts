85bb21e5d36fe4631e84c421ecd2a0d0e20f4471
jhongomes <jhonatangomes859@gmail.com>
2021-07-15 18:57:09 -0400

    Configurando errors no arquivo server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import "reflect-metadata"
+
+
+import express, {Request, Response, NextFunction } from 'express';
+import "express-async-errors"
 
 import "./database"
 
 import "./shared/container";
+
+import { AppError } from './errors/AppError';
 
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
@@ -16,6 +22,20 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
+
+
+app.use((err: Error, request: Request, response:Response, next:NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`,
+    })
+})
 
 
 app.listen(3333, () => console.log('server is start'))
